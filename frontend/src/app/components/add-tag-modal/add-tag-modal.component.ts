808fc9a32cebc5b4ddb8389f68b943dd9d7a7e4d
ZachPoole <ZachPooleWork@gmail.com>
2025-03-29 12:10:44 -0500

    updated stylign for close button on add-tag modal

--- frontend/src/app/components/add-tag-modal/add-tag-modal.component.ts
+++ frontend/src/app/components/add-tag-modal/add-tag-modal.component.ts
@@ -2,10 +2,11 @@ import { Component, input, output } from '@angular/core';
 import { Transaction } from '../../models/models';
 import { TransactionCardComponent } from '../transaction-card/transaction-card.component';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-add-tag-modal',
-  imports: [CommonModule, TransactionCardComponent],
+  imports: [CommonModule, TransactionCardComponent, MatIconModule],
   templateUrl: './add-tag-modal.component.html',
   styleUrl: './add-tag-modal.component.scss',
 })
