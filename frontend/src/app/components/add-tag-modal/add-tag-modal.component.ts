bfc05fa6c99c9b6b958c81784d4d7d66344953f0
ZachPoole <ZachPooleWork@gmail.com>
2025-03-30 14:30:23 -0500

    hooked tags store to add-tag-modal

--- frontend/src/app/components/add-tag-modal/add-tag-modal.component.ts
+++ frontend/src/app/components/add-tag-modal/add-tag-modal.component.ts
@@ -1,10 +1,13 @@
-import { Component, input, output, signal } from '@angular/core';
+import { Component, input, OnInit, output, signal } from '@angular/core';
 import { TransactionCardComponent } from '../transaction-card/transaction-card.component';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { TagComponent } from '../tag/tag.component';
 import { Transaction } from '../../store/transactions/transactions.model';
-import { mockTags } from '../../store/tags/tags.reducers';
+import { initialState, mockTags } from '../../store/tags/tags.reducers';
+import { select, Store } from '@ngrx/store';
+import { selectTags } from '../../store/tags/tags.selectors';
+import { TagsActions } from '../../store/tags/tags.actions';
 
 @Component({
   selector: 'app-add-tag-modal',
@@ -17,11 +20,18 @@ import { mockTags } from '../../store/tags/tags.reducers';
   templateUrl: './add-tag-modal.component.html',
   styleUrl: './add-tag-modal.component.scss',
 })
-export class AddTagModalComponent {
-  tags = signal(mockTags);
+export class AddTagModalComponent implements OnInit {
+  tags$: any;
 
   transactionSelected = input.required<Transaction>();
   closeModalClicked = output();
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.tags$ = this.store.select(selectTags);
+    this.store.dispatch(TagsActions.tagsRetreived({ tags: initialState }));
+  }
 
   closeClicked() {
     this.closeModalClicked.emit();
